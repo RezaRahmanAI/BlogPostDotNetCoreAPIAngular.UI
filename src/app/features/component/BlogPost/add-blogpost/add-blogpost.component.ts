0232944c6c95491a097cc0789af89e4946f07ae3
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2024-02-12 07:54:50 +0600

    BlogPost form submit in console

--- src/app/features/component/BlogPost/add-blogpost/add-blogpost.component.ts
+++ src/app/features/component/BlogPost/add-blogpost/add-blogpost.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../../../models/BlogPost/add-blogpost-request.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
-  styleUrl: './add-blogpost.component.css'
+  styleUrl: './add-blogpost.component.css',
 })
 export class AddBlogpostComponent {
+  model: AddBlogPost;
 
+  constructor() {
+    this.model = {
+      title: '',
+      description: '',
+      content: '',
+      urlHandle: '',
+      featureImgUrl: '',
+      createdDate: new Date(),
+      author: '',
+      isVisible: false,
+    };
+  }
+  
+  onSubmit(): void {
+    console.log(this.model);
+  }
 }
---
+++ src/app/features/models/BlogPost/add-blogpost-request.model.ts
@@ -0,0 +1,11 @@
+export interface AddBlogPost {
+    title: string;
+    description: string;
+    content: string;
+    urlHandle: string;
+    featureImgUrl: string;
+    createdDate: Date;
+    author: string;
+    isVisible: boolean;
+  }
+  
